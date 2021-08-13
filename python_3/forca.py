73a197f95c60061136881f42f0ebcec15ee9c8fa
Arthur Phillip Ferreira da Silva <apsilva@BH0349010.STUBH>
2021-08-13 08:51:54 -0400

    menssagens de fim de jogo

--- python_3/forca.py
+++ python_3/forca.py
@@ -2,25 +2,32 @@ import random
 
 def jogar():
     imprime_cabecalho()
+
     palavra_secreta, letras_acertadas = escolhe_palavra()
+    print('Palavra:', *letras_acertadas)
 
     enforcou = False
     acertou = False
     erros = 0
-    tentativas = 6
+    tentativas = 7
+    chutes_anteriores = []
 
-    print('Palavra:', *letras_acertadas)
 
     while(not enforcou and not acertou):
 
         print('Tentaticas restantes:', tentativas-erros)
         chute = le_chute()
 
-        if (chute in palavra_secreta):
-           registra_chute(chute, palavra_secreta,letras_acertadas)
+        if (chute in chutes_anteriores):
+            print(f'A letra {chute} já foi registrada.\nTente novamente')
         else:
-            erros += 1
-    
+            chutes_anteriores.append(chute)
+            if (chute in palavra_secreta):
+               registra_chute(chute, palavra_secreta,letras_acertadas)
+            else:
+                erros += 1
+                desenha_forca(erros)
+
         enforcou = erros == tentativas
         acertou = "_" not in letras_acertadas
 
@@ -28,9 +35,9 @@ def jogar():
 
 
     if (acertou):
-        print('\nVocê ganhou!!\n')
+        imprime_mensagem_vencedor()
     else:
-        print('\nVocê perdeu. :(\n')
+        imprime_mensagem_perdedor(palavra_secreta)
             
     print('Fim do jogo.')
 
@@ -52,7 +59,7 @@ def escolhe_palavra():
     
     return palavra_secreta, letras_acertadas
 
-def le_chute()
+def le_chute():
     chute = input('Qual a letra? ')
     chute = chute.strip()
     chute = chute.upper()
@@ -65,6 +72,72 @@ def registra_chute(chute, palavra_secreta,letras_acertadas):
             letras_acertadas[index] = letra
         index += 1
 
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print(f"A palavra era {palavra_secreta}")
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
 
+def imprime_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros >= 1):
+        print(" |      (_)   ")
+    
+    if(erros >= 4):
+        print(" |      \|/   ")
+    elif(erros >= 3):
+        print(" |      \|    ")
+    elif(erros >= 2):
+        print(" |       |    ")
+    else:
+        print(" |            ")
+
+    if(erros >= 5):
+        print(" |       |    ")
+    else:
+        print(" |            ")
+
+    if (erros >= 7):
+        print(" |      / \   ")
+    elif(erros >= 6):
+        print(" |      /     ")
+    else:
+        print(" |            ")
+
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+    
 if (__name__ == "__main__"):
     jogar()
