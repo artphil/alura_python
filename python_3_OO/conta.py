b8fb4e25a3dd9b0d3a5f6f312f68bdeb308fc8ca
Arthur Phillip Ferreira da Silva <apsilva@BH0349010.STUBH>
2021-08-27 08:27:34 -0400

    variaveis e metodos privados

--- python_3_OO/conta.py
+++ python_3_OO/conta.py
@@ -1,17 +1,49 @@
 
 
 class Conta:
-    def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+    def __init__(self, numero, titular, saldo=0.0, limite=1000.0):
+        self.__numero = numero
+        self.__titular = titular.title()
+        self.__saldo = saldo
+        self.__limite = limite
 
     def deposita(self, valor):
-        self.saldo += valor 
+        self.__saldo += valor 
+
+    def __pode_sacar(self, valor):
+        return valor <= (self.__saldo + self.__limite)
 
     def saca(self, valor):
-        self.saldo += valor 
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+            return True 
+        else:
+            print(f'O valor {valor} esta acima do disponível.')
+            return False 
+    
+    def transfere(self, valor, destino):
+        if (self.saca(valor)):
+            destino.deposita(valor)
 
     def extrato(self):
-        print(f'O saldo é: {self.saldo}')
+        print(f'O saldo de {self.__titular} é: {self.__saldo}\nO valor disponível é: {self.__saldo + self.__limite}')
+    
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, valor):
+        self.__limite = valor
+    
+    @staticmethod
+    def codigo_banco():
+        return '001'
--- python_3_OO/teste.py
+++ python_3_OO/teste.py
@@ -1,12 +1,22 @@
-def cria_conta(numero, titular, saldo, limite):
-    conta = {'numero': numero, 'titular': titular, 'saldo': saldo, 'limite': limite}
-    return conta
+from conta import Conta
 
-def deposita(conta, valor):
-    conta['saldo'] += valor 
 
-def saca(conta, valor):
-    conta['saldo'] += valor 
+print(Conta.codigo_banco())
 
-def extrato(conta):
-    print(f'Saldo é: {conta['saldo']}')
+c1 = Conta(132,'marco antonio')
+c1.extrato()
+c1.deposita(400)
+c1.extrato()
+c1.saca(600)
+c1.extrato()
+c1.saca(1000)
+c1.extrato()
+
+print(c1.limite)
+
+c2 = Conta(123,'HUGO REIS',30,2000)
+c2.extrato()
+
+c1.transfere(100, c2)
+c1.extrato()
+c2.extrato()
